0d1cc5afa557a4026eced67260d181f650622cb9
lihongxu6 <lihongxu6@jd.com>
2019-11-24 09:26:46 +0800

    bash

--- test/invokesh/1.sh
+++ test/invokesh/1.sh
@@ -2,16 +2,16 @@
 
 A=1
 
-echo "before exec/source/fork: PID for 1.sh = $$"
+echo "执行调用之前进程Id exec/source/fork: PID for 1.sh = $$"
 
 export A
 echo "In 1.sh: variable A=$A"
 
 case $1 in
-        --exec)
+        exec)
                 echo -e "==> using exec…\n"
                 exec ./2.sh ;;
-        --source)
+        source)
                 echo -e "==> using source…\n"
                 . ./2.sh ;;
         *)
@@ -19,5 +19,5 @@ case $1 in
                 ./2.sh ;;
 esac
 
-echo "after exec/source/fork: PID for 1.sh = $$"
-echo -e "In 1.sh: variable A=$A\n"
+echo "执行调用之后进程Id exec/source/fork: PID for 1.sh = $$"
+echo -e "父脚本1.sh: variable A=$A\n"
--- test/invokesh/2.sh
+++ test/invokesh/2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo "PID for 2.sh = $$"
+echo "子脚本2PID for 2.sh = $$"
 echo "In 2.sh get variable A=$A from 1.sh"
 
 A=2
 export A
 
-echo -e "In 2.sh: variable A=$A\n"
+echo -e "子脚本2.sh默认A=2: variable A=$A\n"
