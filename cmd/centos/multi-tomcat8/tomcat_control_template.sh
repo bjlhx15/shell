22ca68ba809b829f38bc7595f79a2b356dd05956
lihongxu6 <lihongxu6@jd.com>
2019-12-09 10:39:18 +0800

    bash

--- cmd/centos/multi-tomcat8/tomcat_control_template.sh
+++ cmd/centos/multi-tomcat8/tomcat_control_template.sh
@@ -4,6 +4,11 @@
 
 cd "$(dirname $0)"/.. || exit 1
 
+
+#JAVA_OPTS参数根据需要添加，尤其是内存相关参数，并且注意这里的设置会覆盖分组设置中的同名值
+export JAVA_OPTS="-Xms1024m -Xmx1024m \
+ -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=UTF-8 -Des.set.netty.runtime.available.processors=false"
+
 export CATALINA_BASE="__CATALINA_BASE__"
 export CATALINA_HOME="__CATALINA_HOME__"
 
