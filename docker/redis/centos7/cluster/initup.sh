bc93d92dfb7148e4f298e1f8fcb25bb885de2d54
lihongxu6 <lihongxu6@jd.com>
2020-02-24 16:36:25 +0800

    password

--- docker/redis/centos7/cluster/initup.sh
+++ docker/redis/centos7/cluster/initup.sh
@@ -36,7 +36,7 @@ echo "开始构建redis集群目录..."
 
 for port in `seq ${redis_port_range}`; do \
   mkdir -p ${redis_dir}/${port}/conf \
-  && PORT=${port} IP=${ip} envsubst < ./redis-cluster.tmpl > ${redis_dir}/${port}/conf/redis.conf \
+  && PORT=${port} IP=${ip} redis_password=${redis_password} envsubst < ./redis-cluster.tmpl > ${redis_dir}/${port}/conf/redis.conf \
   && mkdir -p ${redis_dir}/${port}/data; \
   echo "构建配置文件：${redis_dir}/${port}/conf/redis.conf..."
   echo "构建数据目录：${redis_dir}/${port}/data..."
@@ -65,7 +65,7 @@ for port in `seq ${redis_port_range}`; do \
   docker run -d -ti -p ${port}:${port} -p 1${port}:1${port} \
   -v ${redis_dir}/${port}/conf/redis.conf:/usr/local/etc/redis/redis.conf \
   -v ${redis_dir}/${port}/data:/data \
-  --restart always --name redis-${port} --requirepass "RedisTest_1234" --net redis-net \
+  --restart always --name redis-${port} --net redis-net \
   --sysctl net.core.somaxconn=1024 redis:${redis_version} redis-server /usr/local/etc/redis/redis.conf; \
   #获取docker分配的ip
   execsh=${execsh}`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' redis-${port}`:${port}' '
